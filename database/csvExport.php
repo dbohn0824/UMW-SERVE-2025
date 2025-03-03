8e5501a56bd5239c3de1fd0cc214cf3956ba01c3
dbohn0824 <dbohn@mail.umw.edu>
2025-03-03 10:28:27 -0500

    Added code for additional sections of the csv so you can see the total hours by volunteer, still need to fix a bug where it isn't showing all entries between given dates

--- database/csvExport.php
+++ database/csvExport.php
@@ -1,5 +1,6 @@
 <?php
-
+header('Content-Type: text/csv; charset=utf-8');
+header('Content-Disposition: attachment; filename="export.csv"');
 
 include_once('dbinfo.php');
 
@@ -10,12 +11,14 @@ $con = connect();
 //Get start and end dates from the form submission from exportData.php
 
 
-//check the POST array if the user has submitted a form 
+//check the POST array if the user has submitted a form *************************************
 $startDate = isset($_POST['startDate']) ? $_POST['startDate'] : '';
 
 $endDate = isset($_POST['endDate']) ? $_POST['endDate'] : ''; 
 
-$query = "SELECT * FROM dbpersonhours WHERE eventDate BETWEEN ? and ? ORDER BY personID ASC";
+
+
+$query = "SELECT * FROM dbpersonhours WHERE date BETWEEN ? and ? ORDER BY date ASC";
 
 $stmt = $con->prepare($query);
 
@@ -27,16 +30,46 @@ $result = $stmt->get_result();
 
 $row = $result->fetch_assoc(); 
 
-header('Content-Type: text/csv; charset=utf-8');
-header('Content-Disposition: attachment; filename="export.csv"');
+//*********************************************************************************************
 
+
+//Export volunteer data by date to csv file***********************************************
+
+//open a file for outputing the csv data
 $output = fopen('php://output', 'w');
 
 //output the column headers
 fputcsv($output, array_keys($row)); 
 
+
+//output the row data
 while($row = $result->fetch_assoc()){
     fputcsv($output, $row);
+}
+
+//********************************************************************************************* */
+
+//seperate the sections
+
+for($i = 0; $i < 25; $i = $i + 1 ){
+    fputcsv($output, []);
+}
+
+
+
+//querry to get total hours per person 
+$query = "SELECT personID, SUM(TIMESTAMPDIFF(SECOND, Time_in, Time_out)) / 3600 AS Total_hours FROM dbpersonhours GROUP BY personID";
+
+$stmt = $con->prepare($query);
+
+$stmt->execute(); 
+
+$result = $stmt->get_result();
+
+fputcsv($output, ['Volunteer ID', 'Total Hour']);
+while($row = $result->fetch_assoc()){
+    fputcsv($output, $row);
+    fputcsv($output, []);
 }
 
 
