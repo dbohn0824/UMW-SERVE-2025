6c54af44fda6c639561d9499dee879b68abb5a40
amlewers <119716361+amlewers@users.noreply.github.com>
2025-03-19 09:39:36 -0400

    Update dbPersons.php

--- database/dbPersons.php
+++ database/dbPersons.php
@@ -37,7 +37,29 @@ function add_person($person) {
             $person->get_email() . '","' .
             $person->get_password() . '");'
         );*/
-        mysqli_query($con, 'INSERT INTO dbpersons VALUES ("' .
+        mysqli_query($con, 'INSERT INTO dbpersons (id, first_name, last_name, minor, total_hours, remaining_mandated_hours, checked_in, phone1, email, notes, type, password, street_address, city, state, zip_code, emergency_contact_first_name, emergency_contact_last_name, emergency_contact_phone, emergency_contact_relation) VALUES ("' .
+            $person->get_id() . '","' . 
+            $person->get_first_name() . '","' .
+            $person->get_last_name() . '","' .
+            $person->isMinor() . '","' .
+            $person->get_total_hours() . '","' .
+            $person->get_remaining_mandated_hours() . '","' .
+            $person->get_checked_in() . '","' .
+            $person->get_phone1() . '","' .
+            $person->get_email() . '","' .
+            'n/a' . '","' .
+            $person->get_type() . '","' .
+            $person->get_password() . '","' .
+            $person->get_street_address() . '","' .
+            $person->get_city() . '","' .
+            $person->get_state() . '","' .
+            $person->get_zip_code() . '","' .
+            $person->get_emergency_contact_first_name() . '","' .
+            $person->get_emergency_contact_last_name() . '","' .
+            $person->get_emergency_contact_phone() . '","' .
+            $person->get_emergency_contact_relation() . '")'
+            );
+        /*mysqli_query($con, 'INSERT INTO dbpersons VALUES ("' .
             $person->get_id() . '","' . 
             $person->get_first_name() . '","' .
             $person->get_last_name() . '","' .
@@ -48,16 +70,16 @@ function add_person($person) {
             $person->get_phone1() . '","' .
             $person->get_email() . '","' .
             'n/a' . '","' . /* ("notes", we don't use this) */
-            $person->get_type() . '","' .
-            $person->get_password() . '","' .
-            $person->get_street_address() . '","' .
-            $person->get_city() . '","' .
-            $person->get_state() . '","' .
-            $person->get_zip_code() . '","' .
-            $person->get_emergency_contact_first_name() . '","' .
-            $person->get_emergency_contact_last_name() . '","' .
-            $person->get_emergency_contact_phone() . '","' .
-            $person->get_emergency_contact_relation() . '","'
+         //   $person->get_type() . '","' .
+           // $person->get_password() . '","' .
+            //$person->get_street_address() . '","' .
+            //$person->get_city() . '","' .
+            //$person->get_state() . '","' .
+            //$person->get_zip_code() . '","' .
+           // $person->get_emergency_contact_first_name() . '","' .
+           // $person->get_emergency_contact_last_name() . '","' .
+           // $person->get_emergency_contact_phone() . '","' .
+           // $person->get_emergency_contact_relation() . '","'
             //$person->get_start_date() . '","' .
             //"n/a" . '","' . /* ("venue", we don't use this) */
             //$person->get_phone1type() . '","' .
@@ -86,7 +108,7 @@ function add_person($person) {
             //$person->get_orientation_date() . '","' .
             //$person->get_background_complete() . '","' .
             //$person->get_background_date() . '");'
-        );
+      //  );
         mysqli_close($con);
         return true;
     }
