0e8bc3e815e85b0ea0ece3fd313238fb8537956e
dbohn0824 <dbohn@mail.umw.edu>
2025-04-04 06:56:17 -0400

    deleted js script from checkVolunteerStatus  since its in its own file in filter.js now.

--- checkVolunteerStatus.php
+++ checkVolunteerStatus.php
@@ -50,30 +50,6 @@
    
 
 ?>
-<!--<script>
-
-function filterFunction() {
-  // Declare variables
-  var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("Filtername");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
-  tr = table.getElementsByTagName("tr");
-
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[1];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    }
-  }
-}
-</script>-->
 <script src="js/filter.js"></script>
 <!DOCTYPE html>
 <html>
